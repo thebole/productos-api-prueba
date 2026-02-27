c995f22b4f1c91a50e9223c4d29980e84a7fae8c
Emmanuel Ortega <kenpomuaythai@gmail.com>
2026-02-27 15:03:29 -0400

    responsable de obtener lista de products

--- app/Responsable/Api/Product/AllProductResponsable.php
+++ app/Responsable/Api/Product/AllProductResponsable.php
@@ -2,20 +2,34 @@
 
 namespace App\Responsable\Api\Product;
 
+use App\Repositories\Product\ProductRepository;
 use Illuminate\Contracts\Support\Responsable;
+use Symfony\Component\HttpFoundation\Response;
 
 class AllProductResponsable implements Responsable
 {
+
+    private $repositories;
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(ProductRepository $productRepository)
     {
         //
+        $this->repositories = (object) [
+            'productRepository'=> $productRepository
+        ];
     }
 
     public function toResponse($request)
     {
-        //
+        // 
+        
+        $allProducts = $this->repositories->productRepository->allProducts($request->perPage, $request->page);
+        return response()->json([
+            'products' => $allProducts,
+            'page'  => $request->page,
+            'perPage' => $request->perPage
+        ], Response::HTTP_OK);
     }
 }
